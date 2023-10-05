6fdda80ce4e5444ca8a2f3173f4a46286d78bba5
kushagrasahani <96765538+kushagrasahani@users.noreply.github.com>
2023-10-05 21:33:13 +0530

    Update prime.cc

--- prime.cc
+++ prime.cc
@@ -1,29 +1,29 @@
-///////////////////////////////////////////////////////////////////////////////
-//
-//  This implments a brute force method of determining the prime numbers
-//  in a given range 1..n.
-//
-///////////////////////////////////////////////////////////////////////////////
+// ///////////////////////////////////////////////////////////////////////////////
+// //
+// //  This implments a brute force method of determining the prime numbers
+// //  in a given range 1..n.
+// //
+// ///////////////////////////////////////////////////////////////////////////////
 
-#ifndef PRIME_H_
-#define PRIME_H_
+// #ifndef PRIME_H_
+// #define PRIME_H_
 
-#include	<stdio.h>
-#include	<stdlib.h>
-#include	<math.h>
+// #include	<stdio.h>
+// #include	<stdlib.h>
+// #include	<math.h>
 
-bool is_prime(int num) {
-  int limit, factor = 2;
-  limit = (long) (sqrtf((float) num) + 0.5f);
-  while ((factor <= limit) && (num % factor))
-    factor++;
-  return (factor > limit);
-}
+// bool is_prime(int num) {
+//   int limit, factor = 2;
+//   limit = (long) (sqrtf((float) num) + 0.5f);
+//   while ((factor <= limit) && (num % factor))
+//     factor++;
+//   return (factor > limit);
+// }
 
-int get_first_prime(int k) {
-  while (!is_prime(k))
-    k++;
-  return k;
-}
+// int get_first_prime(int k) {
+//   while (!is_prime(k))
+//     k++;
+//   return k;
+// }
 
-#endif
+// #endif
